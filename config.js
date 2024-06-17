9ac7937c25426e98224cbd4fafeadb0f34e77614
CODER KID <127951183+0CODERKID@users.noreply.github.com>
2024-06-17 22:20:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    TOKEN: "",
-    ownerID: ["1004206704994566164", ""], 
+    TOKEN: "MTI1MjI4NzQwNzE2MDY5Mjc4OQ.GhD-kc.jnOZTF3L2z4DJ9vQRd7gW9Mf2QRz25E17hNfZ0",
+    ownerID: ["1125003820552958003", ""], 
     setupFilePath: './commands/setup.json',
     commandsDir: './commands',  
     embedColor: "00fbff",
