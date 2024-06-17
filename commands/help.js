b8339283dc14075cc00944886f2144be437109c5
Coder Kid <rudranshsingh1110@gmail.com>
2024-06-17 22:13:17 +0530

    first commit

---
+++ commands/help.js
@@ -0,0 +1,26 @@
+
+const { EmbedBuilder } = require('discord.js');
+
+
+module.exports = {
+  name: "help",
+  description: "Get information about the bot",
+  permissions: "0x0000000000000800",
+  options: [],
+  run: async (client, interaction) => {
+    try {
+     
+
+      const embed = new EmbedBuilder()
+         .setColor('#0099ff')
+      .setTitle('Music Maniac')
+      .setDescription('Welcome to the Music Bot!\n\n- Here are the available commands:\n\n' +
+        '**/play :** Start playing the songs.\n' +
+        '**/ping :** check bot latency.\n' );
+
+      return interaction.reply({ embeds: [embed] });
+    } catch (e) {
+    console.error(e); 
+  }
+  },
+};
---
+++ config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    TOKEN: "",
+    ownerID: ["1004206704994566164", ""], 
+    setupFilePath: './commands/setup.json',
+    commandsDir: './commands',  
+    embedColor: "00fbff",
+    errorLog: "", 
+  
+  }
+  
+  
+  
